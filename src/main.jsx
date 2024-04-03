54950532bb4db74645c11183f69250799176620c
Paulo Ricardo <pr741234@gmail.com>
2024-04-03 17:41:45 -0300

    feat: Configura roteamento com react-router-dom

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+import Home from "./routes/Home.jsx";
+import TasksByName from "./routes/TasksByName.jsx";
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/task-name",
+        element: <TasksByName />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
